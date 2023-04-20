f44f965fe3b0fb4e2be0e18d3f3e61fe3cbd1bb4
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-04-20 10:43:32 -0500

    add support for inline template angular

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    opts.ignore_install = { "help" }
-  end,
+  dependencies = {
+    { "elgiano/nvim-treesitter-angular", branch = "topic/jsx-fix" },
+  },
 }
