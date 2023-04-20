f3ad2a2387a474b967ac27b708cd714aba942495
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-04-20 10:43:08 -0500

    add new colorschemas

---
+++ lua/plugins/colorschemas.lua
@@ -0,0 +1,19 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "Shatur/neovim-ayu",
+    mirage = false,
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    options = {
+      theme = "vscode",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu-dark",
+    },
+  },
+}
