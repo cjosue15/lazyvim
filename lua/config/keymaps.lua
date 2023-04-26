ac0a80413a91fc01052668ddb2bc0ff2a0633e8a
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-04-25 19:31:17 -0500

    add keymaps to fast up and down

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-d>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-u>", { noremap = true })
